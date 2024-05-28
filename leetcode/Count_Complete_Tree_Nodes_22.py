69fa133ef06704917af004f584fda0b0c7631afd
Dean <dean.w1993@gmail.com>
2024-05-28 09:41:09 +0100

    feat: Add solution for Count Complete Tree Nodes problem

---
+++ leetcode/Count_Complete_Tree_Nodes_22.py
@@ -0,0 +1,77 @@
+
+# Definition for a binary tree node.
+from turtle import right
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return 0
+
+        def go_left(curr) -> int:
+            height = 0
+            while curr:
+                height += 1
+                curr = curr.left
+            return height
+
+        def go_right(curr) -> int:
+            height = 0
+            while curr:
+                height += 1
+                curr = curr.right
+            return height
+        
+        left = go_left(root)
+        right = go_right(root)
+        
+        if left == right:
+            return (2 ** left) - 1
+        else:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+
+"""
+222. Count Complete Tree Nodes
+Easy
+Topics
+Companies
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,2,3,4,5,6]
+Output: 6
+Example 2:
+
+Input: root = []
+Output: 0
+Example 3:
+
+Input: root = [1]
+Output: 1
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5 * 104].
+0 <= Node.val <= 5 * 104
+The tree is guaranteed to be complete.
+"""
+
