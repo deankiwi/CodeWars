fddd1be2440cba6638d35e683706916cf2b14b0e
Dean <dean.w1993@gmail.com>
2024-06-04 16:55:30 +0100

    LRUCache implementation WIP

---
+++ leetcode/LRU Cache 146 WIP.py
@@ -0,0 +1,208 @@
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.hashTable = {}
+        self.first = None
+        self.last = None
+
+    def get(self, key: int) -> int:
+        if key in self.hashTable:
+            curr = self.hashTable[key]
+
+            if curr.before:
+                ahead = curr.before
+                after = curr.next
+
+                if after:
+                    after.before = ahead
+                    ahead.next = after
+                    if curr == self.last:
+                        self.last = after
+                else:
+                    ahead.next = None
+
+                    if not ahead.before:
+                        ahead.before = curr
+
+                    self.last = ahead
+
+            return curr.val
+
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        node = LinkedList(value, key)
+
+        if len(self.hashTable) >= self.capacity and key not in self.hashTable:
+            old_key = self.last.key
+            if self.last.before:
+                self.last = self.last.before
+                self.last.next = None
+            else:
+                self.last = node
+
+            del self.hashTable[old_key]
+
+        if self.first:
+            self.first.before = node
+        node.next = self.first
+        self.first = node
+        self.hashTable[key] = node
+        if not self.last:
+            self.last = node
+
+
+class LinkedList:
+    def __init__(self, val, key, before=None, next=None) -> None:
+        self.val = val
+        self.key = key
+        self.before = before
+        self.next = next
+
+
+# lRUCache = LRUCache(2)
+# print(lRUCache.put(1, 1))
+# print(lRUCache.put(2, 2))
+# print(lRUCache.get(1))
+# print(lRUCache.put(3, 3))
+# print(lRUCache.get(2))
+# print(lRUCache.put(4, 4))
+# print(lRUCache.get(1))
+# print(lRUCache.get(3))
+# print(lRUCache.get(4))
+
+# lRUCache = LRUCache(1)
+# print(lRUCache.put(1, 1))
+# print(lRUCache.put(2, 2))
+# print(lRUCache.get(1))
+# print(lRUCache.put(3, 3))
+# print(lRUCache.get(2))
+# print(lRUCache.put(4, 4))
+# print(lRUCache.get(1))
+# print(lRUCache.get(3))
+# print(lRUCache.get(4))
+
+lRUCache = LRUCache(2)
+print(lRUCache.get(2))
+print(lRUCache.put(2, 6))
+print(lRUCache.get(1))
+print(lRUCache.put(1, 5))
+print(lRUCache.put(1, 2))
+print(lRUCache.get(1))
+print(lRUCache.get(2))
+
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+"""
+146. LRU Cache
+Medium
+Topics
+Companies
+Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+Implement the LRUCache class:
+
+LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+int get(int key) Return the value of the key if the key exists, otherwise return -1.
+void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+The functions get and put must each run in O(1) average time complexity.
+
+ 
+
+Example 1:
+
+Input
+["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+Output
+[null, null, null, 1, null, -1, null, -1, 3, 4]
+
+Explanation
+LRUCache lRUCache = new LRUCache(2);
+lRUCache.put(1, 1); // cache is {1=1}
+lRUCache.put(2, 2); // cache is {1=1, 2=2}
+lRUCache.get(1);    // return 1
+lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+lRUCache.get(2);    // returns -1 (not found)
+lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+lRUCache.get(1);    // return -1 (not found)
+lRUCache.get(3);    // return 3
+lRUCache.get(4);    // return 4
+ 
+
+Constraints:
+
+1 <= capacity <= 3000
+0 <= key <= 104
+0 <= value <= 105
+At most 2 * 105 calls will be made to get and put.
+"""
+
+
+'''
+Review, correct design pattern used but mistake in code
+could have used a dummy node at either end of linked list
+in the get function remove the value and place it back at the front of list
+
+class LRUCache:
+    class Node:
+        def __init__(self, key, val):
+            self.key = key
+            self.val = val
+            self.prev = None
+            self.next = None
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.head = self.Node(-1, -1)
+        self.tail = self.Node(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.m = {}
+
+    def addNode(self, newnode):
+        temp = self.head.next
+        newnode.next = temp
+        newnode.prev = self.head
+        self.head.next = newnode
+        temp.prev = newnode
+
+    def deleteNode(self, delnode):
+        prevv = delnode.prev
+        nextt = delnode.next
+        prevv.next = nextt
+        nextt.prev = prevv
+
+    def get(self, key: int) -> int:
+        if key in self.m:
+            resNode = self.m[key]
+            ans = resNode.val
+            del self.m[key]
+            self.deleteNode(resNode)
+            self.addNode(resNode)
+            self.m[key] = self.head.next
+            return ans
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.m:
+            curr = self.m[key]
+            del self.m[key]
+            self.deleteNode(curr)
+
+        if len(self.m) == self.cap:
+            del self.m[self.tail.prev.key]
+            self.deleteNode(self.tail.prev)
+
+        self.addNode(self.Node(key, value))
+        self.m[key] = self.head.next
+'''
+
