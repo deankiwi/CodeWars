7a233e0a21b93ea72e65cc4d796ce27f0d6a6094
Dean <dean.w1993@gmail.com>
2025-12-01 20:46:14 +0000

    day 1 pt 1

--- advent-of-code-2025/day-1/main.py
+++ advent-of-code-2025/day-1/main.py
@@ -1,11 +1,12 @@
-
 from typing import TypedDict, List
+
 
 class Order(TypedDict):
     direction: str
     turns: int
 
-def get_zeros_clicks(start: int, orders: List[Order]) -> int:
+
+def get_zeros_clicks_pt1(start: int, orders: List[Order]) -> int:
     count = 0
     curr = start
     for order in orders:
@@ -20,6 +21,27 @@ def get_zeros_clicks(start: int, orders: List[Order]) -> int:
 
     return count
 
+
+def get_zeros_clicks_pt2(start: int, orders: List[Order]) -> int:
+    count = 0
+    curr = start
+    for order in orders:
+        turns = order["turns"]
+        if order["direction"] == "L":
+            turns *= -1
+        if turns > 0:
+            crossings = (curr + turns) // 100 - curr // 100
+        elif turns < 0:
+            crossings = (curr - 1) // 100 - (curr + turns - 1) // 100
+        else:
+            crossings = 0
+
+        count += crossings
+        curr = (curr + turns) % 100
+
+    return count
+
+
 def txt_to_orders(filepath: str) -> List[Order]:
     orders = []
     with open(filepath, "r") as file:
@@ -28,16 +50,19 @@ def txt_to_orders(filepath: str) -> List[Order]:
 
     return orders
 
+
 def main():
     orders = txt_to_orders("advent-of-code-2025/day-1/example.txt")
     # print(orders)
-    
-    print("example: " , get_zeros_clicks(50, orders))
+
+    print("example: ", get_zeros_clicks_pt1(50, orders))
+    print("example: ", get_zeros_clicks_pt2(50, orders))
 
     orders = txt_to_orders("advent-of-code-2025/day-1/actual.txt")
     # print(orders)
-    
-    print("actual: " , get_zeros_clicks(50, orders))
 
+    print("actual: ", get_zeros_clicks_pt1(50, orders))
+    print("actual: ", get_zeros_clicks_pt2(50, orders))
 
-main()
+if __name__ == "__main__":
+    main()
